c0726ac4bbf84e3e5cd146bf7898438a8817de17
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 04:04:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do | seasons, holiday_name |
     if seasons == :winter 
       holiday_name.each do | holiday, supplies |
-        supplies[supply]
+        supplies << supply
       end
     end
   end
