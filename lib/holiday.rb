bb369b3c344627ca6cbb2e45b00d949f4b9b986a
Zeshawn Khan <zeshawnk@gmail.com>
2018-09-12 04:19:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do | season, holiday |
-    return season.capitlize
+    return "#{season.capitlize}:"
+    
     
 
 end
